8774720fb9ec5a96b76838b643d7dd0208501257
Pasquale Lisena <pasquale.lisena@eurecom.fr>
2020-01-30 17:14:23 +0100

    fix

--- server.py
+++ server.py
@@ -21,6 +21,8 @@ db_tracking = TinyDB('database/tracking.json')
 flask_app = Flask(__name__)
 api = Api(app=flask_app,
           version="0.1.0",
+          doc='/api',
+          prefix='/api',
           title="Face Recognition Api",
           description="Recognise celebrities on videos.", )
 CORS(flask_app)
