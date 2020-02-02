a2c9cf0ec877de3dc1f86afed3dc48840495f1b1
Pasquale Lisena <pasquale.lisena@eurecom.fr>
2020-02-02 11:22:41 +0100

    fix

--- visualizer/src/components/AnnotatedVideo.vue
+++ visualizer/src/components/AnnotatedVideo.vue
@@ -128,8 +128,7 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 video {
-  max-width: 100vw;
-  max-height: 100vh;
+  width: 100%;
   margin: auto;
 }
 .video-container {
