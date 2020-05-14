06986f883f52c68974e835716b252f93da26ee95
Pasquale Lisena <pasquale.lisena@eurecom.fr>
2020-05-14 12:06:01 +0200

    update grlc uri

--- visualizer/src/services/memad.js
+++ visualizer/src/services/memad.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = 'http://grlc.eurecom.fr/api/MeMAD-project/api/program_detail';
+const API = 'http://grlc.eurecom.fr/api-git/MeMAD-project/api/program_detail';
 
 async function get(url) {
   const data = await axios.get(API, {
