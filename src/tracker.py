aa2cbf5186b5b9ce92cdae12a6983c14208fa533
Pasquale Lisena <pasquale.lisena@eurecom.fr>
2020-07-22 15:03:04 +0200

    tracker as a class

--- src/tracker.py
+++ src/tracker.py
@@ -10,7 +10,7 @@ import src.database as database
 from .FaceRecogniser import Classifier
 from .FaceAligner import FaceAligner
 from .SORT.sort import Sort
-from .utils import utils, uri_utils, media_fragment
+from .utils import utils, media_fragment
 from .utils.face_utils import judge_side_face
 
 colours = np.random.rand(32, 3)
@@ -46,144 +46,154 @@ def parse_fragment(fragment, fps):
 
 
 def main(video_path, project='general', video_speedup=25, export_frames=False, fragment=None, video_id=None):
-    cluster_features = True
-
     if not video_id:
         video_id = video_path
-    video_capture = cv2.VideoCapture(video_path)
+    t = Tracker(project)
+    t.run(video_path, video_speedup, export_frames, fragment, video_id)
 
-    # setup all paths
-    output_path = utils.generate_output_path('./data/out', project, video_id)
-    cluster_path = os.path.join(output_path, 'cluster')
-    frames_path = os.path.join(output_path, 'frames')
-    if export_frames:
-        os.makedirs(frames_path, exist_ok=True)
-    classifier_path = os.path.join('data/classifier', project + '.pkl')
-    trackers_csv = os.path.join(output_path, 'trackers.csv')
-    predictions_csv = os.path.join(output_path, 'predictions.csv')
 
-    # init csv outputs
-    trackers_writer = init_csv(trackers_csv, ['x1', 'y1', 'x2', 'y2', 'track_id', 'frame'])
-    predictions_writer = init_csv(predictions_csv, ['x1', 'y1', 'x2', 'y2', 'track_id', 'name',
-                                                    'confidence', 'frame', 'tracker_sample', 'npt'])
+class Tracker:
+    def __init__(self, project='general'):
+        self.project = project
+        classifier_path = os.path.join('data/classifier', project + '.pkl')
+        self.classifier = Classifier(classifier_path)
+        self.aligner = FaceAligner(desiredFaceWidth=160, margin=10)
+        self.detector = MTCNN(min_face_size=25)
 
-    classifier = Classifier(classifier_path)
-    classifier.collect_features = cluster_features
-    aligner = FaceAligner(desiredFaceWidth=160, margin=10)
+    def run(self, video_path, video_speedup=25, export_frames=False, fragment=None, video_id=None):
+        cluster_features = True  # TODO parametrise
 
-    detector = MTCNN(min_face_size=25)
+        video_capture = cv2.VideoCapture(video_path)
 
-    # init tracker
-    tracker = Sort(min_hits=0)
+        # setup all paths
+        output_path = utils.generate_output_path('./data/out', self.project, video_id)
+        cluster_path = os.path.join(output_path, 'cluster')
+        frames_path = os.path.join(output_path, 'frames')
 
-    # frames per second
-    fps = video_capture.get(cv2.CAP_PROP_FPS)
-    video_length = int(video_capture.get(cv2.CAP_PROP_FRAME_COUNT))
-    width = int(video_capture.get(cv2.CAP_PROP_FRAME_WIDTH))
+        if export_frames:
+            os.makedirs(frames_path, exist_ok=True)
 
-    scale_rate = 0.9 if width > 700 else 1
+        trackers_csv = os.path.join(output_path, 'trackers.csv')
+        predictions_csv = os.path.join(output_path, 'predictions.csv')
 
-    frame_start = 0
-    frame_end = video_length
-    if fragment is not None:
-        frame_start, frame_end = parse_fragment(fragment, fps)
+        # init csv outputs
+        trackers_writer = init_csv(trackers_csv, ['x1', 'y1', 'x2', 'y2', 'track_id', 'frame'])
+        predictions_writer = init_csv(predictions_csv, ['x1', 'y1', 'x2', 'y2', 'track_id', 'name',
+                                                        'confidence', 'frame', 'tracker_sample', 'npt'])
 
-    matches = []
-    # iterate over the frames
-    for frame_no in np.arange(frame_start, frame_end, video_speedup):
-        print('frame %d/%d' % (frame_no, frame_end))
-        video_capture.set(cv2.CAP_PROP_POS_FRAMES, frame_no)
+        self.classifier.collect_features = cluster_features
 
-        # read the frame
-        ret, frame = video_capture.retrieve()
+        # init tracker
+        tracker = Sort(min_hits=0)
 
-        if frame is None:
-            raise RuntimeError
+        # frames per second
+        fps = video_capture.get(cv2.CAP_PROP_FPS)
+        video_length = int(video_capture.get(cv2.CAP_PROP_FRAME_COUNT))
+        width = int(video_capture.get(cv2.CAP_PROP_FRAME_WIDTH))
 
-        face_list = []
-        attribute_list = []
-        frame = cv2.resize(frame, (0, 0), fx=scale_rate, fy=scale_rate)
-        frame_height, frame_width, _ = frame.shape
-        rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        rgb_frame = cv2.cvtColor(rgb_frame, cv2.COLOR_GRAY2RGB)
-        img_size = np.asarray(frame.shape)[0:2]
-        bounding_boxes = detector.detect_faces(rgb_frame)
+        scale_rate = 0.9 if width > 700 else 1
 
-        # print('Detected %d faces' % len(bounding_boxes))
-        for item in bounding_boxes:
-            bb = utils.xywh2rect(*utils.fix_box(item['box']))
-            face_list.append(bb)
+        frame_start = 0
+        frame_end = video_length
+        if fragment is not None:
+            frame_start, frame_end = parse_fragment(fragment, fps)
 
-            # use 5 face landmarks to judge the face is front or side
-            facial_landmarks = list(item['keypoints'].values())
-            dist_rate, high_ratio_variance, width_rate = judge_side_face(facial_landmarks)
-            # dist_rate 0 => front face ; 1 => side face
+        matches = []
+        # iterate over the frames
+        for frame_no in np.arange(frame_start, frame_end, video_speedup):
+            print('frame %d/%d' % (frame_no, frame_end))
+            video_capture.set(cv2.CAP_PROP_POS_FRAMES, frame_no)
 
-            # face cropped
-            cropped = frame.copy()[bb[1]:bb[3], bb[0]:bb[2], :]
+            # read the frame
+            ret, frame = video_capture.retrieve()
 
-            attribute_list.append([cropped, item['confidence'], dist_rate, high_ratio_variance, width_rate])
+            if frame is None:
+                raise RuntimeError
 
-        trackers = tracker.update(np.array(face_list), img_size, cluster_path, attribute_list, rgb_frame)
-        tracker_sample = tracker.frame_count
-        # this is a counter of the frame analysed by the tracker (so normalised respect to the video_speedup)
+            face_list = []
+            attribute_list = []
+            frame = cv2.resize(frame, (0, 0), fx=scale_rate, fy=scale_rate)
+            frame_height, frame_width, _ = frame.shape
+            rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            rgb_frame = cv2.cvtColor(rgb_frame, cv2.COLOR_GRAY2RGB)
+            img_size = np.asarray(frame.shape)[0:2]
+            bounding_boxes = self.detector.detect_faces(rgb_frame)
 
-        for d in trackers:
-            d = d.astype(int)
-            # the predicted position is outside the image
-            if any(i < 0 for i in d) \
-                    or d[0] >= frame_width or d[2] >= frame_width \
-                    or d[1] >= frame_height or d[3] >= frame_height:
-                print('Error tracker %d at frame %d:' % (d[4], frame_no))
-                continue
+            # print('Detected %d faces' % len(bounding_boxes))
+            for item in bounding_boxes:
+                bb = utils.xywh2rect(*utils.fix_box(item['box']))
+                face_list.append(bb)
 
-            trackers_writer.writerow([str(i) for i in d] + [str(frame_no)])
+                # use 5 face landmarks to judge the face is front or side
+                facial_landmarks = list(item['keypoints'].values())
+                dist_rate, high_ratio_variance, width_rate = judge_side_face(facial_landmarks)
+                # dist_rate 0 => front face ; 1 => side face
 
-            # cutting the img on the face
-            trackers_cropped = aligner.align(frame[d[1]:d[3], d[0]:d[2], :])
+                # face cropped
+                cropped = frame.copy()[bb[1]:bb[3], bb[0]:bb[2], :]
 
-            boxname = str(frame_no) + "_" + str(d[0]) + "_" + str(d[1]) + "_" + str(d[2]) + "_" + str(d[3])
-            best_name, best_prob = classifier.predict_best(trackers_cropped, boxname)
+                attribute_list.append([cropped, item['confidence'], dist_rate, high_ratio_variance, width_rate])
 
-            npt = utils.frame2npt(frame_no, fps)
-            predictions_writer.writerow(
-                [str(i) for i in d] + [best_name, best_prob, str(frame_no), tracker_sample, npt])
+            trackers = tracker.update(np.array(face_list), img_size, cluster_path, attribute_list, rgb_frame)
+            tracker_sample = tracker.frame_count
+            # this is a counter of the frame analysed by the tracker (so normalised respect to the video_speedup)
 
-            # apply back the scale rate
-            box = [x / scale_rate for x in d[0:4].tolist()]
-            match = {
-                'name': best_name,
-                'project': project,
-                'track_id': int(d[4]),
-                'frame': int(frame_no),
-                'confidence': best_prob,
-                'tracker_sample': tracker_sample,
-                'npt': npt,
-                'locator': video_id,
-                'bounding': utils.rect2xywh(*box),
-                'rect': box
-            }
-            matches.append(match)
-            if database.is_on():
-                database.insert_partial_analysis(match)
+            for d in trackers:
+                d = d.astype(int)
+                # the predicted position is outside the image
+                if any(i < 0 for i in d) \
+                        or d[0] >= frame_width or d[2] >= frame_width \
+                        or d[1] >= frame_height or d[3] >= frame_height:
+                    print('Error tracker %d at frame %d:' % (d[4], frame_no))
+                    continue
 
-            if export_frames:
-                export_frame(frame, d, best_name, frame_no, frames_path)
+                trackers_writer.writerow([str(i) for i in d] + [str(frame_no)])
 
-    # TODO final track
+                # cutting the img on the face
+                trackers_cropped = self.aligner.align(frame[d[1]:d[3], d[0]:d[2], :])
 
-    if database.is_on():
-        print('COMPLETE')
-        database.save_status(video_id, project, 'COMPLETE')
+                boxname = str(frame_no) + "_" + str(d[0]) + "_" + str(d[1]) + "_" + str(d[2]) + "_" + str(d[3])
+                best_name, best_prob = self.classifier.predict_best(trackers_cropped, boxname)
 
-    for f in file_to_be_close:
-        f.close()
+                npt = utils.frame2npt(frame_no, fps)
+                predictions_writer.writerow(
+                    [str(i) for i in d] + [best_name, best_prob, str(frame_no), tracker_sample, npt])
 
-    if cluster_features:
-        clus = classifier.cluster_features(5, 3, 4)
-        print(clus)
+                # apply back the scale rate
+                box = [x / scale_rate for x in d[0:4].tolist()]
+                match = {
+                    'name': best_name,
+                    'project': self.project,
+                    'track_id': int(d[4]),
+                    'frame': int(frame_no),
+                    'confidence': best_prob,
+                    'tracker_sample': tracker_sample,
+                    'npt': npt,
+                    'locator': video_id,
+                    'bounding': utils.rect2xywh(*box),
+                    'rect': box
+                }
+                matches.append(match)
+                if database.is_on():
+                    database.insert_partial_analysis(match)
 
-    return matches
+                if export_frames:
+                    export_frame(frame, d, best_name, frame_no, frames_path)
+
+        # TODO final track
+
+        if database.is_on():
+            print('COMPLETE')
+            database.save_status(video_id, self.project, 'COMPLETE')
+
+        for f in file_to_be_close:
+            f.close()
+
+        if cluster_features:
+            clus = self.classifier.cluster_features(5, 3, 4)
+            print(clus)
+
+        return matches
 
 
 def parse_args():
@@ -206,6 +216,8 @@ def parse_args():
 
 if __name__ == '__main__':
     args = parse_args()
+    from .utils import uri_utils
+
     video = uri_utils.normalize_video(args.video)
 
     main(video, args.project, args.video_speedup, args.export_frames)
