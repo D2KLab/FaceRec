64983095a1165278c4d5780271d7e7fbfed26d1f
Pasquale Lisena <pasquale.lisena@eurecom.fr>
2020-07-30 14:12:31 +0200

    options

--- src/crawler.py
+++ src/crawler.py
@@ -13,7 +13,7 @@ from .utils import utils
 logger = logging.getLogger('crawler')
 
 
-def main(keyword, max_num=50, project='general'):
+def main(keyword, max_num=50, project='general', discard_multi_face=False, debug=True):
     if not keyword:
         raise ValueError('Keyword parameter is required.')
 
@@ -21,13 +21,16 @@ def main(keyword, max_num=50, project='general'):
 
     logger.info('[%s] Crawler run for: %s' % (project, keyword))
     print('[%s] Crawler run for: %s' % (project, keyword))
-
     image_dir = os.path.expanduser(os.path.join('data/training_img/', project, keyword.replace(" ", "_")))
     os.makedirs(image_dir, exist_ok=True)
     al_image_dir = os.path.expanduser(os.path.join('data/training_img_aligned/', project, keyword.replace(" ", "_")))
     os.makedirs(al_image_dir, exist_ok=True)
 
-    google_crawler = GoogleImageCrawler(feeder_threads=10, parser_threads=10, log_level=logging.DEBUG,
+    if debug:
+        log_level = logging.DEBUG
+    else:
+        log_level = None
+    google_crawler = GoogleImageCrawler(feeder_threads=10, parser_threads=10, log_level=log_level,
                                         downloader_threads=25, storage={'root_dir': image_dir})
     # filters = dict(type='photo')  # I find photo more accurate than 'face'
     start = time.time()
@@ -41,6 +44,8 @@ def main(keyword, max_num=50, project='general'):
         print(filename)
         image = utils.load_gray(os.path.join(image_dir, f))
         extracted_faces = detector.extract(image)
+        if discard_multi_face and len(extracted_faces) > 1:
+            continue
         for i, face in enumerate(extracted_faces):
             output_filename = os.path.join(al_image_dir, '%s_%d.png' % (filename, i))
             cv2.imwrite(output_filename, face)
@@ -57,9 +62,11 @@ def parse_arguments(argv):
                         help='Max number of images to download')
     parser.add_argument("--project", type=str, default='general',
                         help='Name of the collection to be part of')
+    parser.add_argument("--discard_multi_faces", default=False, action='store_true',
+                        help='If set, discard the pictures that contain more that one face.')
     return parser.parse_args(argv)
 
 
 if __name__ == '__main__':
     args = parse_arguments(sys.argv[1:])
-    main(args.keyword, args.max_num, args.project)
+    main(args.keyword, args.max_num, args.project, args.discard_multi_faces)
