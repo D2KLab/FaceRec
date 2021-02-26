c02846fbb734fe75575c836e7d70cc8eec8c6d35
Pasquale Lisena <pasquale.lisena@eurecom.fr>
2021-02-26 16:08:34 +0100

    add example video

--- visualizer/src/components/Home.vue
+++ visualizer/src/components/Home.vue
@@ -6,6 +6,7 @@ import antract from '@/services/antract';
 
 const EXAMPLES = {
   memad: 'http://data.memad.eu/yle/a-studio/8a3a9588e0f58e1e40bfd30198274cb0ce27984e',
+  memad_gt: 'http://data.memad.eu/yle/eurovaalit-2019-kuka-johtaa-eurooppaa/d9d05488b35db559cdef35bac95f518ee0dda76a',
   antract: 'http://www.ina.fr/media/AFE86004832',
 };
 
